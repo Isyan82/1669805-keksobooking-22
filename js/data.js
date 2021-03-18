d1df3745ecc2c49c482340cbd44f204a150ee395
Isyan <isyan@mail.ru>
2021-03-18 17:47:36 +0300

    вроде сделал домашку 7-1/2,

--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 import {showAlert} from './util.js'
 
 const getAds = () => {
-  return fetch('https://22.javascript.pages.academy/keksobooking/dat')
+  return fetch('https://22.javascript.pages.academy/keksobooking/data')
     .then((response) => response.json())
     .catch(() => {showAlert('При загрузке данных с сервера произошла ошибка запроса')})
 };
