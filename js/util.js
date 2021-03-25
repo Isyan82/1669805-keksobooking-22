d57ff7555751a557db0b88bca0dfbf70b03a1ac2
Isyan <isyan@mail.ru>
2021-03-25 09:38:23 +0300

    поправил 8-3

--- js/util.js
+++ js/util.js
@@ -39,10 +39,10 @@ const makeUniqueRandomIntegerGenerator = (min, max) => {
 };
 // функция склонения окончаний
 const transformEnding = (number, textForms) => {
-  number = Math.abs(number) % 100; const n1 = number % 10;
+  number = Math.abs(number) % 100; const number1 = number % 10;
   if (number > 10 && number < 20) { return textForms[2]; }
-  if (n1 > 1 && n1 < 5) { return textForms[1]; }
-  if (n1 == 1) { return textForms[0]; }
+  if (number1 > 1 && number1 < 5) { return textForms[1]; }
+  if (number1 == 1) { return textForms[0]; }
   return textForms[2];
 }
 
