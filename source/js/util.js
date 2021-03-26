c14e4cdcd196a055b37a6b3a88f213c08f317e32
Isyan <isyan@mail.ru>
2021-03-26 13:54:28 +0300

    Упаковал проект в web pack(домашка 9-1)

--- source/js/util.js
+++ source/js/util.js
@@ -42,7 +42,7 @@ const transformEnding = (number, textForms) => {
   number = Math.abs(number) % 100; const number1 = number % 10;
   if (number > 10 && number < 20) { return textForms[2]; }
   if (number1 > 1 && number1 < 5) { return textForms[1]; }
-  if (number1 == 1) { return textForms[0]; }
+  if (number1 === 1) { return textForms[0]; }
   return textForms[2];
 }
 
